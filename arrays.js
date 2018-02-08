5946dbf944f50d229f812686a1bbc94a9c104a34
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-08 02:02:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,30 +1,33 @@
 var chocolateBars =  ["snickers",
 "hundred grand",
 "kitkat",
-"skittles"]
+"skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  var array2 = [element, ...array]
-  return array2
+  var array2 = [element, ...array];
+  return array2;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
-  return array
+  return array;
 }
 
 function addElementToEndOfArray(array, element){
-  var array3 = [...array, element]
-  return array3
+  var array3 = [...array, element];
+  return array3;
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index){
-  return array[2]
+  return array[2];
 }
 
-function
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array
+}
