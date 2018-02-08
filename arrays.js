d5516baf2d95c70654bef08b27b980b67f29d5fb
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-08 02:10:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,10 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift();
-  return array
+  return array;
 }
 
+function removeElementFromBeginningOfArray(array){
+  var array4 = array.slice(1);
+  return array4;
+}
